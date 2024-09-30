42ef72701df88ea2a7a596fb8e287afc4b2b941a
Mariano Schemmari <mariano@truckatlas.com>
2024-09-30 10:44:39 -0300

    add implementation for web {wip}

--- index.js
+++ index.js
@@ -7,7 +7,3 @@ import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-devsettings';
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  initialProps: {},
-  rootTag: document.getElementById('app-root'),
-});
